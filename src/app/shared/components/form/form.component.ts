04b5774e195032dc907e4413bd0a658a756b77ab
vkdelchev <btwonu@gmail.com>
2024-10-27 23:34:58 +0200

    add watchlist todo

--- src/app/shared/components/form/form.component.ts
+++ src/app/shared/components/form/form.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
+import { WatchlistService } from '../../services/watchlist/watchlist.service';
 
 @Component({
   selector: 'app-form',
@@ -12,7 +13,11 @@ import { MatSelectModule } from '@angular/material/select';
   styleUrls: ['./form.component.scss'],
 })
 export class FormComponent implements OnInit {
-  constructor() {}
+  constructor(private watchlistService: WatchlistService) {}
 
   ngOnInit(): void {}
+
+  // addWatchlist() {
+  //   this.watchlistService.addToWatchlist(); // TODO: pass movie id
+  // }
 }
