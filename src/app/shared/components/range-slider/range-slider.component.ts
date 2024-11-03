3155eba4da4287cc9bb0c1c93ad0dcef268f2a4a
vkdelchev <btwonu@gmail.com>
2024-11-03 19:04:47 +0200

    feat: add range slider styles

--- src/app/shared/components/range-slider/range-slider.component.ts
+++ src/app/shared/components/range-slider/range-slider.component.ts
@@ -3,13 +3,21 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NouisliderModule } from 'ng2-nouislider';
 import { FormControl } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-range-slider',
   templateUrl: './range-slider.component.html',
   styleUrls: ['./range-slider.component.scss'],
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, NouisliderModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    NouisliderModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
 })
 export class RangeSliderComponent implements OnInit {
   @Input() label = '';
