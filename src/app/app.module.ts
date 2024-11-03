e1f02e2739d69389c27992ab9a782c27c4eb0988
vkdelchev <btwonu@gmail.com>
2024-11-03 21:56:09 +0200

    refactor: extract header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { FormsModule } from '@angular/forms';
 import { TmdbInterceptor } from './shared/services/tmdb/tmdb-interceptor';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @NgModule({
   declarations: [AppComponent, PageNotFoundComponent],
@@ -29,6 +31,8 @@ import { TmdbInterceptor } from './shared/services/tmdb/tmdb-interceptor';
     MatIconModule,
     MatSnackBarModule,
     FormsModule,
+    HeaderComponent,
+    FooterComponent,
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: TmdbInterceptor, multi: true },
